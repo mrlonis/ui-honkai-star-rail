5b1e21d1bd029df40ea87145ed41532f9fa7c229
Matthew Lonis <mattlonis@gmail.com>
2023-12-05 20:51:34 -0500

    Adding character specific page

---
+++ app/[name]/page.tsx
@@ -0,0 +1,29 @@
+import { Character } from '@/components/characters-response'
+
+// TODO - Eventually we'll want to remove this when the backend is deployed somewhere
+export const dynamic = 'force-dynamic'
+
+async function getCharacter(name: string): Promise<Character> {
+  const res = await fetch(`http://localhost:9003/api/v2/character?name=${name}`, { next: { revalidate: 1 } })
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
+
+  return res.json()
+}
+
+export default async function Character({ params }: { params: { name: string } }) {
+  const character = await getCharacter(params.name)
+
+  return (
+    <div>
+      <p>
+        Character (ID: {character.id}) (Name: {params.name})
+      </p>
+    </div>
+  )
+}
